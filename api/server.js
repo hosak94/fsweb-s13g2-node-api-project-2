1b92c0afdfdfb8c3dc2bd21e41e0d32061cb1cd8
hosak94 <yigitcanhosak@gmail.com>
2023-05-19 02:17:22 +0300

    complete course work

--- api/server.js
+++ api/server.js
@@ -1,3 +1,13 @@
 // server için gerekli olanları burada ayarlayın
 
+const express = require("express");
+const server = express();
+const postRouter = require("./posts/posts-router");
+
+server.use(express.json()); //json istekleri desteklemesi için.
+
+server.use("/api/posts", postRouter);
+
 // posts router'ını buraya require edin ve bağlayın
+
+module.exports = server;
